9c7b408484fdbc84322ec2921753736436afc04e
NicolasM0301 <nicolasmoreno0331@gmail.com>
2024-10-17 18:24:46 -0400

    Updated python and java file

---
+++ practice.java
@@ -0,0 +1,27 @@
+/*
+ * Testing different languages in one repo
+ */
+
+import java.util.Random;
+
+public class practice {
+
+        public static Random myrand = new Random();
+        
+
+public static void main (String [] args){
+
+    int num1 = myrand.nextInt(0,20);
+    int num2 = myrand.nextInt(20,40);
+
+    System.out.println(num1 + num2);
+}
+
+
+
+
+
+
+
+
+}
